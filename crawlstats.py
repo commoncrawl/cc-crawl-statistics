5d803433ffe818ba426a7bbe1ad13539d6c5702d
Sebastian Nagel <sebastian@commoncrawl.org>
2018-01-25 15:43:43 +0100

    Update plots for January 2018 crawl (CC-MAIN-2015-05)

--- crawlstats.py
+++ crawlstats.py
@@ -76,6 +76,7 @@ class MonthlyCrawl:
                'CC-MAIN-2017-43': 29,
                'CC-MAIN-2017-47': 30,
                'CC-MAIN-2017-51': 31,
+               'CC-MAIN-2018-05': 32,
                }
 
     by_id = dict(map(reversed, by_name.items()))

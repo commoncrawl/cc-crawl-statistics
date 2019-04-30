674feb75df4505af50f0690016477c728938a9ee
Sebastian Nagel <sebastian@commoncrawl.org>
2019-04-30 15:07:40 +0200

    Update plots for April 2019 crawl (CC-MAIN-2019-18)

--- crawlstats.py
+++ crawlstats.py
@@ -95,6 +95,7 @@ class MonthlyCrawl:
                'CC-MAIN-2019-04': 44,
                'CC-MAIN-2019-09': 45,
                'CC-MAIN-2019-13': 46,
+               'CC-MAIN-2019-18': 47,
                }
 
     by_id = dict(map(reversed, by_name.items()))
--- plot/table.py
+++ plot/table.py
@@ -124,8 +124,8 @@ class TabularStats(CrawlPlot):
         data = data.groupby(['crawl', 'type']).agg({'pages': 'sum'})
         data = data.groupby(level=0).apply(lambda x: 100.0*x/float(x.sum()))
         data = data.reset_index()
-        field_percentage_formatter = '{0:,.4f}'.format
-        data.to_csv('data/' + base_name + '_percentage.csv', float_format='%.4f')
+        data.to_csv('data/' + base_name + '_percentage.csv',
+                    float_format='%.4f')
 
     def plot(self, crawls, name, column_header, xtra_css_classes=[]):
         # stats comparison for selected crawls

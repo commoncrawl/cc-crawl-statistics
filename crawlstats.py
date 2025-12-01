086d23d668ed6ca36c41283fa36f532e09dc6017
Thom Vaughan <thom@commoncrawl.org>
2025-12-01 13:10:54 +0000

    chore: register CC-MAIN-2025-51 in crawl enum

--- crawlstats.py
+++ crawlstats.py
@@ -154,6 +154,7 @@ class MonthlyCrawl:
                'CC-MAIN-2025-38': 115,
                'CC-MAIN-2025-43': 116,
                'CC-MAIN-2025-47': 117,
+               'CC-MAIN-2025-51': 118,
     }
 
     by_id = dict(map(reversed, by_name.items()))

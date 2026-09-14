3c9c1cde542ca11b26cffb0752b1803e168fd104
Hande Celikkanat <7702228+handecelikkanat@users.noreply.github.com>
2026-09-14 19:16:40 +0300

    fix(domain.py): readd save_data

--- plot/domain.py
+++ plot/domain.py
@@ -21,6 +21,12 @@ class DomainStats(TabularStats):
         path = self.TOP_DOMAINS_FILE.format(self.crawl, self.MAX_TOP_DOMAINS)
         self.type_stats = pandas.read_csv(path)
 
+    def save_data(self, name):
+        """Write the top domains csv next to the html table."""
+        self.type_stats.to_csv('{}/{}-top-{}.csv'.format(
+                                self.PLOTDIR, name, self.MAX_TOP_DOMAINS),
+                               float_format='%.6f', index=None)
+
     def plot(self, name):
         data = self.type_stats
         css_classes = ['tablesorter', 'tablesearcher']
@@ -42,4 +48,5 @@ if __name__ == '__main__':
     plot_name = 'domains'
     plot = DomainStats(latest_crawl)
     plot.read_data()
+    plot.save_data(plot_name)
     plot.plot(plot_name)

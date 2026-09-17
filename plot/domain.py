61d211facdf2c9a1386bd56d31649fe1b137c1d5
Hande Celikkanat <7702228+handecelikkanat@users.noreply.github.com>
2026-09-17 17:00:39 +0300

    feat(domain.py,plot.sh): move calculation of percentages here from crawl workflow

--- plot/domain.py
+++ plot/domain.py
@@ -2,7 +2,7 @@ import sys
 
 import pandas
 
-from crawlstats import MonthlyCrawl
+from crawlstats import CST, MonthlyCrawl
 from plot.table import TabularStats
 
 
@@ -16,10 +16,27 @@ class DomainStats(TabularStats):
         super().__init__()
         self.crawl = crawl
 
+    def add(self, key, val):
+        """Collect the crawl size records read from stdin."""
+        cst = CST[key[0]]
+        if cst != CST.size:
+            return
+        if key[2] != self.crawl:
+            return
+        self.size[key[1]] = val
+
     def read_data(self):
         """Read the downloaded top domains csv."""
         path = self.TOP_DOMAINS_FILE.format(self.crawl, self.MAX_TOP_DOMAINS)
         self.type_stats = pandas.read_csv(path)
+
+    def transform_data(self):
+        """Add the percentage columns, counts are crawl totals."""
+        data = self.type_stats
+        for cnt in ['pages', 'urls']:
+            total = self.size[cnt[:-1]]
+            data['%' + cnt] = 100.0 * data[cnt] / total
+        self.type_stats = data
 
     def save_data(self, name):
         """Write the top domains csv next to the html table."""
@@ -47,6 +64,8 @@ if __name__ == '__main__':
     latest_crawl = plot_crawls[-1]
     plot_name = 'domains'
     plot = DomainStats(latest_crawl)
+    plot.read_from_stdin_or_file()
     plot.read_data()
+    plot.transform_data()
     plot.save_data(plot_name)
     plot.plot(plot_name)

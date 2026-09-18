9551a3baf7806fbb48729e034649d1f1cd82e86d
Hande Celikkanat <7702228+handecelikkanat@users.noreply.github.com>
2026-09-18 15:59:57 +0300

    feat(get_stats.sh): streamline s3 paths so they can be overriden for test

--- get_stats.sh
+++ get_stats.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+# Download crawl statistics and top-domains csv files into ./stats/
+#
+# Usage: ./get_stats.sh
+# Test: To test top-domains feature, override TOP_DOMAINS_S3/URL, eg.
+#           TOP_DOMAINS_S3=s3://<test-bucket>/<test-prefix>/crawl-analysis ./get_stats.sh
+
 set -o pipefail
 
 TOP_DOMAINS_FILE=domains-top-1000-extended.csv
 TOP_DOMAINS_FOLDER=./stats/top-domains
 
-if aws s3 ls s3://commoncrawl/crawl-analysis/ | sed -E 's@.* @@; s@/$@@' >./stats/crawls.txt; then
+CRAWL_ANALYSIS_S3=s3://commoncrawl/crawl-analysis
+CRAWL_ANALYSIS_URL=https://data.commoncrawl.org/crawl-analysis
+
+# Override to test, see above.
+TOP_DOMAINS_S3=${TOP_DOMAINS_S3:-$CRAWL_ANALYSIS_S3}
+TOP_DOMAINS_URL=${TOP_DOMAINS_URL:-$CRAWL_ANALYSIS_URL}
+
+
+if aws s3 ls ${CRAWL_ANALYSIS_S3}/ | sed -E 's@.* @@; s@/$@@' >./stats/crawls.txt; then
     ON_AWS=true;
     echo "Running on AWS (AWS CLI configured for authenticated access)"
 else
-    echo "Downloading from https://data.commoncrawl.org/ using curl"
+    echo "Downloading from ${CRAWL_ANALYSIS_URL} using curl"
     # list of crawls enumerated in crawlstats.py
     python3 -c 'from crawlstats import MonthlyCrawl; [print(c) for c in sorted(MonthlyCrawl.by_name.keys())]' >./stats/crawls.txt
     ON_AWS=false
@@ -22,17 +36,17 @@ while read crawl; do
     if [ -e stats/$crawl.gz ]; then
         echo "  ... stats exist"
     elif $ON_AWS; then
-        aws s3 cp s3://commoncrawl/crawl-analysis/$crawl/stats/part-00000.gz ./stats/$crawl.gz
+        aws s3 cp ${CRAWL_ANALYSIS_S3}/$crawl/stats/part-00000.gz ./stats/$crawl.gz
     else
-        curl --silent https://data.commoncrawl.org/crawl-analysis/$crawl/stats/part-00000.gz >./stats/$crawl.gz
+        curl --silent ${CRAWL_ANALYSIS_URL}/$crawl/stats/part-00000.gz >./stats/$crawl.gz
     fi
     # top-domains csv might be missing for older crawls, in that case continue without failing
     TOP_DOMAINS_TARGET=${TOP_DOMAINS_FOLDER}/${crawl}.${TOP_DOMAINS_FILE}
     if [ -e ${TOP_DOMAINS_TARGET} ]; then
         echo "  ... top-domains exist"
     elif $ON_AWS; then
-        aws s3 cp s3://commoncrawl/crawl-analysis/$crawl/stats/${TOP_DOMAINS_FILE} ${TOP_DOMAINS_TARGET}
+        aws s3 cp ${TOP_DOMAINS_S3}/$crawl/stats/${TOP_DOMAINS_FILE} ${TOP_DOMAINS_TARGET}
     else
-        curl --silent --fail https://data.commoncrawl.org/crawl-analysis/$crawl/stats/${TOP_DOMAINS_FILE} -o ${TOP_DOMAINS_TARGET} || rm -f ${TOP_DOMAINS_TARGET}
+        curl --silent --fail ${TOP_DOMAINS_URL}/$crawl/stats/${TOP_DOMAINS_FILE} -o ${TOP_DOMAINS_TARGET} || rm -f ${TOP_DOMAINS_TARGET}
     fi
 done <./stats/crawls.txt
